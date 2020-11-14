1bb795b2ed40b5e3db04ced325639d9d5fb089d7
Jacob Sowanick <sowanicj@oregonstate.edu>
2020-11-13 17:01:07 -0800

    Fixed error with image destination and added some minor error handling

--- src/CameraModule.py
+++ src/CameraModule.py
@@ -14,13 +14,17 @@ while True:
         break
     if (userc == 'c'):
         d = datetime.now()
-        timename = "~/Pictures/"
+        timename = "/home/pi/Pictures/"
         timename = timename + str(d.year) + str(d.month) + str(d.day) + "_"
         timename = timename + str(d.hour) + str(d.minute) + str(d.second)
         timename = timename + str(d.microsecond) + ".jpg"
-        camera.capture(timename)
-        print("Capture Saved to \"" + timename + "\"")
+        try:
+            camera.capture(timename)
+            print("Capture Saved to \"" + timename + "\"")
+        except:
+            print("There was an error capturing \"" + timename + "\"!")
+            break
     else:
         print("Sorry, input \"" + userc + "\" unrecognisable")
-        
+
 camera.stop_preview()
