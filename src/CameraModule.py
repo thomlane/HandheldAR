2dbbfb44604d629f2fd40e348e1641b9b3ee82ae
Jacob Sowanick <sowanicj@oregonstate.edu>
2020-11-13 16:21:23 -0800

    Added user input and ability to caputre and save images

--- src/CameraModule.py
+++ src/CameraModule.py
@@ -3,6 +3,24 @@ from time import sleep
 from datetime import datetime
 
 camera = PiCamera()
-camera.start_preview()
-sleep(5)
+camera.start_preview(fullscreen=False, window=(100,20,640,480))
+sleep(1)
+
+while True:
+    userc = input("Input: ")
+    
+    if (userc == 'q'):
+        print("Quitting")
+        break
+    if (userc == 'c'):
+        d = datetime.now()
+        timename = "~/Pictures/"
+        timename = timename + str(d.year) + str(d.month) + str(d.day) + "_"
+        timename = timename + str(d.hour) + str(d.minute) + str(d.second)
+        timename = timename + str(d.microsecond) + ".jpg"
+        camera.capture(timename)
+        print("Capture Saved to \"" + timename + "\"")
+    else:
+        print("Sorry, input \"" + userc + "\" unrecognisable")
+        
 camera.stop_preview()
